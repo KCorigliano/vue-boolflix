e8cd65c5318bb5b9301e98e09bba943296c1a002
KCorigliano <corigliano98@hotmail.com>
2022-02-03 13:21:49 +0100

    cardbox hover effect and style

--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -1,20 +1,33 @@
 <template>
-    <div class="card-container">
-        <p>Titolo: {{title}}</p>
-        <p>Titolo originale: {{originalTitle}}</p>
-        <p>Voto: {{vote}}</p>
-        <p>Overview: {{overview}}</p>
+    <div class="card-container" @mouseover="hover = false" @mouseleave="hover = true">
+        <div class="background" v-if="hover">
+            <img :src="'https://image.tmdb.org/t/p/w500/'+background" alt="">
+        </div>
+        <div class="text" v-else>
+            <p><span>Titolo:</span> {{title}}</p>
+            <p><span>Titolo originale:</span> {{originalTitle}}</p>
+            <p><span>Voto:</span> {{vote}}</p>
+            <p><span>Overview:</span> {{overview}}</p>
+        </div>
+        
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            hover: false,
+        }
+    },
     props:{
         title: String,
         originalTitle: String,
-        vote: String,
+        vote: Number,
         overview: String,
-    }
+        background: String,
+    },
+    
 }
 </script>
 
@@ -22,5 +35,30 @@ export default {
 .card-container{
     width: calc(100% / 5);
     margin: 0 15px 0 0;
+    cursor: pointer;
+
+    .background{
+        img{
+            width: 100%;
+        }
+    }
+
+    .text{
+        padding: 15px;
+        color: white;
+        background-color: black;
+        border: 2px solid white;
+        height: 345px;
+        overflow: auto;
+
+        p{
+            margin-bottom: 15px;
+
+            span{
+                font-weight: bold;
+            }
+        }
+    }
+
 }
 </style>
--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="container">
-        <card-box :title="filmList[0].name" :originalTitle="filmList[0].original_name" :vote="filmList[0].vote_average" :overview="filmList[0].overview"/>
+        <card-box 
+            :title="filmList[0].name" 
+            :originalTitle="filmList[0].original_name" 
+            :vote="filmList[0].vote_average" 
+            :overview="filmList[0].overview"
+            :background="filmList[0].poster_path" />
     </div>
 </template>
 
