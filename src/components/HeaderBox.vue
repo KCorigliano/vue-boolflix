af5f63184bf7c04e0a0e5e3c3f43ed046833b4cc
KCorigliano <corigliano98@hotmail.com>
2022-02-03 12:01:30 +0100

    style headerbox

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        
+    <div class="header">
+        <h1>Boolflix</h1>
+        <div>
+            <input type="text" placeholder="Cerca un film">
+            <button>Search</button>
+        </div>
     </div>
 </template>
 
@@ -11,5 +15,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.header{
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    padding: 0 15px;
+    height: 50px;
 
+    h1{
+        color: red;
+        text-transform: uppercase;
+    }
+
+    div{
+        input{
+            margin-right: 15px;
+        }
+    }
+}
 </style>
